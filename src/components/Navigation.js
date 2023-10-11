615371e2691ecb26db1f3ffc70d73c94915d50e8
nosianuri <nurinosia@gmail.com>
2023-10-11 22:11:32 +0600

    basic setup

---
+++ src/components/Navigation.js
@@ -0,0 +1,13 @@
+import React, { Fragment, useState, useEffect } from 'react';
+import { Dialog, Transition} from '@headlessui/react';
+import { XMarkIcon} from '@heroicons/react/24/outline';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+const Navigation = () => {
+  return (
+    <div>Navigation</div>
+  )
+}
+
+export default Navigation
